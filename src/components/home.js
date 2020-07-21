9e151b481b7258e774447841f42e98925c07ffa4
Kevin Roi Basina <kevinroirigorbasina@pm.me>
2020-07-21 17:23:46 +0800

    added input handler

--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import useForm from '../helpers/useForm';
 import Nav from './extras/nav';
 import homeStyles from '../styles/style-home';
 
 const Home = props => {
     const { classes } = props;
+    const [input, handleInput, handleReset] = useForm('')
 
     return (
         <article>
             <Nav />
 
             <form className={classes.form} noValidate autoComplete="off">
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                    value={input}
+                    onChange={handleInput}
+                />
             </form>
 
         </article>
